65c38ed5679fd0bb6cc004e240579a27e7c297fe
gorbynet <michael.roberts@iop.org>
2016-12-23 13:41:06 +0000

    Update test.pl
    Added all required perl modules

--- perl/test.pl
+++ perl/test.pl
@@ -2,11 +2,17 @@
 
 use strict;
 
-# use CGI qw /:standard/; 
-# use LWP::UserAgent; 
-# use Data::Dumper;
-# use URI::Escape;
-# use HTML::Entities;
+use CGI qw /:standard/; 
+use Cwd;
+use Data::Dumper;
+use Encode qw(decode encode);
+use HTML::Entities;
+use LWP::Simple qw/get/;
+use LWP::UserAgent;
+use MIME::Lite;
+use POSIX;
+use URI::Escape;
+
 
 print <<END_OF_HTML;
 Status: 200 OK
@@ -15,8 +21,20 @@ Content-type: text/html
 <HTML>
 <HEAD><TITLE>Everything is going to be OK</TITLE></HEAD>
 <BODY>
-  <H1>Error</H1>
+  <H1>All is good</H1>
   <P>Perl works</P>
+  <ul>
+  <li>use CGI qw /:standard/; </li>
+  <li>use Cwd;</li>
+  <li>use Data::Dumper;</li>
+  <li>use Encode qw(decode encode);</li>
+  <li>use HTML::Entities;</li>
+  <li>use LWP::Simple qw/get/;</li>
+  <li>use LWP::UserAgent;</li>
+  <li>use MIME::Lite;</li>
+  <li>use POSIX;</li>
+  <li>use URI::Escape;</li>
+  </ul>
 </BODY>
 </HTML>
 END_OF_HTML
