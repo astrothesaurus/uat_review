52481c591779030cf067263f0725c25300eaace7
gorbynet <michael.roberts@iop.org>
2016-12-31 21:54:45 +0000

    Update reference to corichi

--- html/js/uat_autocomplete.js
+++ html/js/uat_autocomplete.js
@@ -3,7 +3,7 @@ $(function() {
 
 	$( "#search_term" ).autocomplete(
 	{
-		 source:"http://corichi/cgi-bin/uat_query.pl",
+		 source:"http://localhost/cgi-bin/uat_query.pl",
 		 minLength:2
 	});
 });	
